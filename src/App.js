4c609904c26c8ab2b9b5ec41d56caafbf54786e1
HussainShafayet <shafayetsec09@gmail.com>
2024-10-20 02:21:00 +0600

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <>
-      CRUD
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-blue-500">Hello, CRUD!</h1>
+      </div>
     </>
   );
 }
